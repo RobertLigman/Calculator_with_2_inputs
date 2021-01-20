c870bc2dc23d1a6be6e4a69ad89e64a1ec04f8ed
Robert Ligman <verxus123@gmail.com>
2021-01-20 16:01:36 +0100

    add radio button and other calculating methods

--- main.js
+++ main.js
@@ -1,9 +1,26 @@
 const firstInput = document.querySelector("[data-label=first-input]");
 const secondInput = document.querySelector("[data-label=second-input]");
 const btn = document.querySelector("[data-label=calculate]");
-const calculationType = document.querySelector("[data-label=calculation-type]");
+
 const output = document.querySelector("[data-label=output]");
 const calculate = () => {
-  output.textContent = +firstInput.value + +secondInput.value;
+  const calculationType = [
+    ...document.querySelectorAll("[name=calculation-type]"),
+  ];
+  const calcType = calculationType.find((item) => item.checked === true);
+  switch (calcType.value) {
+    case "add":
+      output.textContent = +firstInput.value + +secondInput.value;
+      break;
+    case "substract":
+      output.textContent = +firstInput.value - +secondInput.value;
+      break;
+    case "multiple":
+      output.textContent = +firstInput.value * +secondInput.value;
+      break;
+    case "divide":
+      output.textContent = +firstInput.value / +secondInput.value;
+      break;
+  }
 };
 btn.addEventListener("click", calculate);
